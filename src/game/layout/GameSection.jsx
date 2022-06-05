f89cd7384bf4bd72c733bbea7c816a165bd55908
SirPrimrose <Primmtech@gmail.com>
2022-06-04 22:01:35 -0500

    Make PhasePanel a top level element, only showing relevant panels for phase

--- src/game/layout/GameSection.jsx
+++ src/game/layout/GameSection.jsx
@@ -1,12 +1,12 @@
 import { Paper } from '@mui/material';
 import React from 'react';
-import PhasePanel from './PhasePanel';
+import JobsPanel from './phases/wander/JobsPanel';
 
 class GameSection extends React.PureComponent {
   render() {
     return (
       <Paper elevation={8} sx={{ height: '100%' }}>
-        <PhasePanel />
+        <JobsPanel />
       </Paper>
     );
   }
--- src/game/layout/phases/wander/WanderPhase.jsx
+++ src/game/layout/phases/wander/WanderPhase.jsx
@@ -1,77 +1,52 @@
-import { Box, Grid, Stack, Typography } from '@mui/material';
+import { Grid } from '@mui/material';
 import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { getCurrentJobs } from '../../../../slice/gameSlice';
-import JobActions from '../JobActions';
-import { JOB_CATEGORY } from '../../../data/jobs';
-import WorldResourcePanel from '../../WorldResourcePanel';
-import ExploreGroupPanel from '../../ExploreGroupPanel';
+import PlayerSection from '../../PlayerSection';
+import GameSection from '../../GameSection';
+import InfoSection from '../../InfoSection';
 
 class WanderPhase extends React.PureComponent {
-  renderJobCategory = (currentJobs, category) => {
-    const jobsInCategory = currentJobs[category];
-    if (jobsInCategory && jobsInCategory.length > 0) {
-      return (
-        <Box className="panelOutline">
-          <Typography variant="h5" align="center">
-            {category}
-          </Typography>
-          <Box className="panelGrid">
-            <JobActions availableJobs={jobsInCategory} />
-          </Box>
-        </Box>
-      );
-    }
-    return <Box className="flexContainer" />;
-  };
-
   render() {
-    // TODO: Match design template...
-    // "Build" column also contains "Monuments" panel
-    // "Recon" column also contains "World Knowledge" panel
-    const { currentJobs } = this.props;
-
     return (
-      <Grid container columnSpacing={2} px={2} py={1} sx={{ height: '100%' }}>
-        <Grid container item direction="column" xs={4}>
-          <Grid item xs={8} overflow="hidden">
-            {this.renderJobCategory(currentJobs, JOB_CATEGORY.ACTION)}
-          </Grid>
-          <Grid item xs={4} overflow="hidden">
-            <WorldResourcePanel />
-          </Grid>
+      <Grid
+        container
+        sx={{
+          flexGrow: 1,
+          overflow: { xs: 'auto', md: 'hidden' },
+        }}
+      >
+        <Grid
+          item
+          xs={12}
+          md={2.5}
+          lg={2}
+          sx={{ height: { xs: '40%', md: '100%' } }}
+          p={2}
+        >
+          <PlayerSection />
         </Grid>
-        <Grid container item direction="column" xs={4}>
-          <Grid item xs={8} overflow="hidden">
-            {this.renderJobCategory(currentJobs, JOB_CATEGORY.CRAFT)}
-          </Grid>
+        <Grid
+          item
+          xs={12}
+          md={6}
+          lg={7}
+          sx={{ height: { xs: '50%', md: '100%' } }}
+          p={2}
+        >
+          <GameSection />
         </Grid>
-        <Grid container item direction="column" xs={4}>
-          <Grid item xs={8} overflow="hidden">
-            <Stack>
-              {this.renderJobCategory(currentJobs, JOB_CATEGORY.EXPLORATION)}
-              {this.renderJobCategory(currentJobs, JOB_CATEGORY.PROGRESSION)}
-            </Stack>
-          </Grid>
-          <Grid item xs={4} overflow="hidden">
-            <ExploreGroupPanel />
-          </Grid>
+        <Grid
+          item
+          xs={12}
+          md={3.5}
+          lg={3}
+          sx={{ height: { xs: '50%', md: '100%' } }}
+          p={2}
+        >
+          <InfoSection />
         </Grid>
       </Grid>
     );
   }
 }
 
-WanderPhase.propTypes = {
-  // Key-value mappings of categories and the jobs in them
-  currentJobs: PropTypes.objectOf(
-    PropTypes.arrayOf(PropTypes.string.isRequired).isRequired
-  ).isRequired,
-};
-
-const mapStateToProps = (state) => ({
-  currentJobs: getCurrentJobs(state),
-});
-
-export default connect(mapStateToProps)(WanderPhase);
+export default WanderPhase;
