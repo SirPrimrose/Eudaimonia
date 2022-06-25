54765475879b5acf3477c547a3d9c59db2969781
SirPrimrose <Primmtech@gmail.com>
2022-06-25 14:26:32 -0500

    Right click job only does it once

--- src/game/layout/phases/JobActions.jsx
+++ src/game/layout/phases/JobActions.jsx
@@ -1,44 +1,16 @@
-import { Button, LinearProgress, Stack } from '@mui/material';
+import { Stack } from '@mui/material';
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { actions as gameActions } from '../../../slice/gameSlice';
-import { createJobQueueEntry } from '../../data/jobConstructor';
-import { getProgressValue } from '../../../shared/util';
+import Job from '../components/Job';
 
 class JobActions extends React.PureComponent {
-  handleClickJob = (jobName) => () => {
-    const { addJobToQueue, setPaused } = this.props;
-
-    addJobToQueue(createJobQueueEntry(jobName));
-    setPaused(false);
-  };
-
   renderAvailableJobs = () => {
     const { availableJobs } = this.props;
 
     // TODO: Render a job in its own component, pass job data to component
-    return availableJobs.map((job) => (
-      <Stack
-        key={job.id}
-        style={{
-          position: 'relative',
-        }}
-      >
-        <Button onClick={this.handleClickJob(job.id)} sx={{ zIndex: 1 }}>
-          {job.name}
-        </Button>
-        <LinearProgress
-          variant="determinate"
-          value={getProgressValue(job.currentXp, job.maxXp)}
-          sx={{
-            position: 'absolute',
-            width: '100%',
-            bottom: 0,
-          }}
-        />
-      </Stack>
-    ));
+    return availableJobs.map((job) => <Job key={job.id} job={job} />);
   };
 
   render() {
@@ -49,16 +21,13 @@ class JobActions extends React.PureComponent {
 JobActions.propTypes = {
   availableJobs: PropTypes.arrayOf(
     PropTypes.shape({
-      isActive: PropTypes.bool.isRequired,
+      id: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
+      isActive: PropTypes.bool.isRequired,
       currentXp: PropTypes.number.isRequired,
       maxXp: PropTypes.number.isRequired,
     })
   ).isRequired,
-
-  // Dispatch functions
-  addJobToQueue: PropTypes.func.isRequired,
-  setPaused: PropTypes.func.isRequired,
 };
 
 const mapDispatchToProps = {
